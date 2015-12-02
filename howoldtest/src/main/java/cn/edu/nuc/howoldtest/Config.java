1a9bdabb6ec40ec0581d592db7b88016bdc79c58
weifucheng <786318384@qq.com>
2015-12-02 11:19:52 +0800

    download

---
+++ howoldtest/src/main/java/cn/edu/nuc/howoldtest/Config.java
@@ -0,0 +1,10 @@
+package cn.edu.nuc.howoldtest;
+
+/**
+ * Created by lenovo on 2015/9/28.
+ */
+public class Config {
+
+    public static final  String API_KEY="c4a538f2b6ecaff378e076d77976968c";
+    public static final  String API_SECRET="8UM7ud5Y7QeuEBGTBzBTBb3Vr0zA3";
+}
