78582a4bb3b2728aab3ce4fda17e3b0580a2fb98
xmmmmmovo <13256172360@163.com>
2018-05-08 16:45:43 +0800

    给学长看代码

--- SeeWorld/VitamioRecorderSampleFor/src/com/yixia/camera/demo/ui/record/InvalidStreamException.java
+++
@@ -1,5 +0,0 @@
-package com.yixia.camera.demo.ui.record;
-
-public class InvalidStreamException extends Exception {
-	private static final long serialVersionUID = -7287854603988271545L;
-}
--- SeeWorld/app/src/main/java/com/example/seeworld/MainActivity.java
+++ SeeWorld/app/src/main/java/com/example/seeworld/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        checkCertificate(MainActivity.this);
     }
 
     @Override
@@ -43,44 +41,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    /**
-     * 获取md5指纹信息(签名文件不同,md5指纹也不同;不同包名app可以具有相同的md5指纹信息)
-     * 示例输出:
-     * sign: 55:2e:ba:e6:b4:7e:ac:e3:02:58:64:9a:db:82:87:b6
-     */
-    private void checkCertificate(Context context) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            Signature sig = pm.getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
-            String md5Fingerprint = doFingerprint(sig.toByteArray(), "MD5");
-            Log.d("sign:", md5Fingerprint);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * @param certificateBytes 获取到应用的signature值
-     * @param algorithm        在上文指定MD5算法
-     * @return md5签名
-     */
-    private String doFingerprint(byte[] certificateBytes, String algorithm) throws Exception {
-        MessageDigest md = MessageDigest.getInstance(algorithm);
-        md.update(certificateBytes);
-        byte[] digest = md.digest();
-
-        String toRet = "";
-        for (int i = 0; i < digest.length; i++) {
-            if (i != 0) {
-                toRet += ":";
-            }
-            int b = digest[i] & 0xff;
-            String hex = Integer.toHexString(b);
-            if (hex.length() == 1) {
-                toRet += "0";
-            }
-            toRet += hex;
-        }
-        return toRet;
-    }
 }
